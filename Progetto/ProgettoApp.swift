11a6174b6cd8be5985ccb4a4bf75673c321bb266
Matteo Manzoni <matteo.manzoni2001@gmail.com>
2024-06-10 12:51:05 +0200

    Initial Commit

---
+++ Progetto/ProgettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgettoApp.swift
+//  Progetto
+//
+//  Created by Students on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ProgettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
